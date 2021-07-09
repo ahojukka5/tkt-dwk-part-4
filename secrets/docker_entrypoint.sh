bf8b14ee184bb9d5fd5e45b7618fae845984660d
Jukka Aho <ahojukka5@gmail.com>
2021-07-09 19:41:55 +0300

    Remove files from parts 1-3

--- secrets/docker_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while :
-do
-    date
-    echo "Environment variable SECRET1 = $SECRET1"
-    echo "Environment variable SECRET2 = $SECRET2"
-    echo "Press [CTRL+C] to stop.."
-    sleep 1
-done
